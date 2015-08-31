068998f59e564521be847222fc7acb40a262a5b6
unknown <ejohema@5CB3270TZP.intern.hoglandet.se>
2015-08-31 12:07:27 +0200

    PagedArray2D.set(...)/get(...) created(unfinished).

--- PagedArary2D.js
+++ PagedArary2D.js
@@ -4,8 +4,8 @@ GP.PagedArray2D = function(sizeX, sizeY, defaultValue, defaultPageData) {
 	this.sizeY = sizeY;
 	this.defaultValue = defaultValue;
 	this.defaultPageData = defaultPageData;
-	this.pageTable = {};
-	pageTable['0,8'] = Page2D(this.sizex, this.sizeY, defaultValue, defaultPageData);
+	this.pages = {};
+	this.pages['0,8'] = Page2D(this.sizex, this.sizeY, defaultValue, defaultPageData);
 }
 
 GP.Page2D = function(sizeX, sizeY, defaultValue, pageData) {
@@ -17,4 +17,52 @@ GP.Page2D = function(sizeX, sizeY, defaultValue, pageData) {
 		}
 	}
 	
+}
+
+GP.PagedArray2D.prototype.get(x, y, value) {
+	
+	var pageX = x/this.sizeX;
+	var pageY = y/this.sizeY;
+	var localX = x%this.sizeX;
+	var localY = y%this.sizeY;
+	
+	// Fix indexing of negative values:
+	if (x < 0)
+		pageX--;
+	if (x < 0)
+		pageY--;
+	
+	
+	var pagePosString = pageX + "," + pageY;
+	
+	if (!this.pages.hashOwnProperty(pagePosString)) {
+		return this.defaultValue;
+	}
+	
+	return pages[pagePosString].get(localX, localY);
+}
+
+GP.PagedArray2D.prototype.set(x, y, value) {
+	
+	var pageX = x/this.sizeX;
+	var pageY = y/this.sizeY;
+	var localX = x%this.sizeX;
+	var localY = y%this.sizeY;
+	
+	// Fix indexing of negative values:
+	if (x < 0)
+		pageX--;
+	if (x < 0)
+		pageY--;
+		
+	var pagePosString = pageX + "," + pageY;
+	
+	if (!this.pages.hashOwnProperty(pagePosString)) {
+		Page2D page = Page2D(this.sizeX, this.sizeY, this.defaultValue);
+		page.set(localX, localY, value);
+		pages[pagePosString] = page;
+	}
+	else {
+		pages[pagePosString].set(localX, localY, value);
+	}
 }
