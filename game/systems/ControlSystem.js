f588c9d513733809a5f8734722245e8eeb5a96d1
Gustav Hammar <gustav9797@hotmail.se>
2015-09-04 22:56:18 +0200

    normalisering fungerar ej

--- game/systems/ControlSystem.js
+++ game/systems/ControlSystem.js
@@ -14,33 +14,101 @@ ECS.Systems.ControlSystem = CES.System.extend({
 			controlledplayer.oldvy = physics.vy;
 			controlledplayer.oldRot = Math.round(physics.rotation * 100) / 100;
 			
-			//physics.vx = 0;
-			//physics.vy = 0;
-			
-			var moveSpeed = 2;
+			var dx = physics.x - (game.camera.pos.x + mousex);
+			var dy = physics.y - (game.camera.pos.y + mousey);
 		
-			if (keyboard.isKeyDown("left"))
-			{
-				physics.vx += -moveSpeed;
-			}
-			else if (keyboard.isKeyDown("right"))
-			{
-				physics.vx += moveSpeed;
-			}
+			var angle = Math.atan2(dy, dx);
+			physics.rotation = angle + 1.5 * Math.PI;
+			player.sprite.rotation = physics.rotation;
+			
+			//console.log(physics.rotation);
+			
+			var moveSpeed = 3;
+			var moveAngle = angle +  Math.PI;
+			var numKeys = 0;
+		
+			// Noob code for keys begin
 			if (keyboard.isKeyDown("up"))
 			{
-				physics.vy += -moveSpeed;
+				if (keyboard.isKeyDown("left"))
+				{
+					moveAngle -= Math.PI * 0.25;
+				}
+				else if (keyboard.isKeyDown("right"))
+				{
+					moveAngle += Math.PI * 0.25;
+				}
 			}
 			else if (keyboard.isKeyDown("down"))
 			{
-				physics.vy += moveSpeed;
+				moveAngle += Math.PI;
+				if (keyboard.isKeyDown("left"))
+				{
+					moveAngle += Math.PI * 0.25;
+				}
+				else if (keyboard.isKeyDown("right"))
+				{
+					moveAngle -= Math.PI * 0.25;
+				}	
+			} 
+			else if (keyboard.isKeyDown("left")) 
+			{
+				moveAngle += Math.PI * 1.5;
+				if (keyboard.isKeyDown("up"))
+				{
+					moveAngle += Math.PI * 0.25;
+				}
+				else if (keyboard.isKeyDown("down"))
+				{
+					moveAngle -= Math.PI * 0.25;
+				}
 			}
-		
-			var angle = Math.atan2(physics.y - (game.camera.pos.y + mousey), physics.x - (game.camera.pos.x + mousex));
-			//console.log("xpos " + physics.x + " ypos " + physics.y + " camx " + GP.camera.frustrum.x + " camy " + GP.camera.frustrum.y);
-			//console.log(GP.camera);
-			physics.rotation = angle + 1.5 * Math.PI;
-			player.sprite.rotation = physics.rotation;
+			else if (keyboard.isKeyDown("right"))
+			{
+				moveAngle += Math.PI * 0.5;
+				if (keyboard.isKeyDown("up"))
+				{
+					moveAngle -= Math.PI * 0.25;
+				}
+				else if (keyboard.isKeyDown("down"))
+				{
+					moveAngle += Math.PI * 0.25;
+				}	
+			}
+			if(keyboard.isKeyDown("left") ||
+				keyboard.isKeyDown("right") ||
+				keyboard.isKeyDown("up") ||
+				keyboard.isKeyDown("down")) {
+					
+				var vec = new b2Vec2(Math.cos(moveAngle), Math.sin(moveAngle));
+				//console.log(moveAngle);
+				//console.log(vector.Length());
+				//var normalizedX = Math.cos(moveAngle) / vector.Length();
+				//var normalizedY = Math.sin(moveAngle) / vector.Length();
+				
+				if(vec.Length() != 0 && vec != NaN && vec != undefined) {
+					console.log(vec);
+					if(true) {
+						var normalizedVector = new b2Vec2(vec.x / vec.Length(), vec.y / vec.Length());    
+						//new b2Vec2(normalizedX, normalizedY);
+						console.log(normalizedVector);
+							
+						if(normalizedVector != NaN && normalizedVector != undefined) {
+							physics.vx += moveSpeed * normalizedVector.x;
+							physics.vy += moveSpeed * normalizedVector.y;
+						}
+					}
+				}
+				
+				//console.log("t1 " + t1 + " t2 " + t2);
+					
+				//physics.vx += moveSpeed * Math.sin(moveAngle)/Math.abs(Math.sin(moveAngle)) * Math.sqrt(1 - Math.pow(Math.sin(moveAngle), 2));
+				//physics.vy += moveSpeed * Math.sin(moveAngle)/Math.abs(Math.sin(moveAngle)) * Math.sqrt(1 - Math.pow(Math.cos(moveAngle), 2));
+				
+				//console.log("total speed " + ((-moveSpeed * Math.cos(moveAngle))+(-moveSpeed * Math.sin(moveAngle))));
+				//console.log("movespeed " + Math.sqrt(Math.pow(moveSpeed * Math.cos(moveAngle), 2) + Math.pow(moveSpeed * Math.sin(moveAngle), 2)));
+			}
+			// Noob code for keys end
 			
 			// Check if anything changed, if so, send player update packet
 			if(controlledplayer.oldx != physics.x 
