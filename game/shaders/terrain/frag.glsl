96921a69a05eddec60baeb5ffc94864edf0239bf
ostkaka <john.ostkaka@gmail.com>
2015-09-05 18:28:50 +0200

    Terrain rendering looks better. ;-)

--- game/shaders/terrain/frag.glsl
+++ game/shaders/terrain/frag.glsl
@@ -48,7 +48,7 @@ void main() {
 	
 	//highp float dis = 0.5+0.25*raymarch(vec3(uv*16.0, 0.0));
 	highp float density = getDensity(uv);
-	highp float alpha = clamp(32.0*(density-0.5), 0.0, 1.0);
+	highp float alpha = 0.5+0.5*clamp(32.0*(density-0.5), 0.0, 1.0);
 	highp vec3 color = texture2D(texture, uv).xyz;
 	
 	gl_FragColor = vec4(color*alpha, alpha);
