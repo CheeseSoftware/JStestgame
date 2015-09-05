2c31c1a57f07a456a7a6d4eb23cb97ecad54d64d
ostkaka <john.ostkaka@gmail.com>
2015-09-05 19:04:15 +0200

    .

--- game/Game.js
+++ game/Game.js
@@ -299,11 +299,11 @@ Game.prototype.initializeListeners = function() {
 		addChat(data.message);
 	});
 	
-	this.connection.on('dig', function(data) {
+	this.connection.on('dig', function(data, context) {
 		var x = data.x;
 		var y = data.y;
 		var digRadius = data.digRadius;
-		this.terrain.fillCircle(x/32.0, y/32.0, digRadius);
+		context.terrain.fillCircle(x/32.0, y/32.0, digRadius);
 		//TODO: Change terrain
-	});
+	}, this);
 }
