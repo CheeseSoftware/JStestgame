e5e644a61b473c42518b501cc715ea826c26e1f5
Gustav Hammar <gustav9797@hotmail.se>
2016-03-15 22:34:52 +0100

    Remove unused lib

--- index.php
+++ index.php
@@ -36,8 +36,6 @@
     <script src="lib/perlin.js"></script>
     <script src="lib/jquery-2.1.x.js"></script>
     <script src="lib/bootstrap.min.js"></script> <!-- For progressbar -->
-    <!--<script src="lib/three.js"></script>-->
-    <script src="lib/webgl-debug.js"></script>
     
     <?php 
         if ($debug != "true") {
